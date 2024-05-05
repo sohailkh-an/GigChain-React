d19fe212075881a3dafc79e70cedbe527a8c1b73
Sohail Khan <Pindigamer@gmail.com>
2024-05-06 02:05:45 +0500

    Fixed the cross-origin properties to enable get, post and delete operation to go through with the origin url

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,48 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-  // origin: 'http://localhost:5173', 
-  origin: 'https://gigchain-frontend.vercel.app', 
-  optionsSuccessStatus: 200
-  };
-  
-  app.use(cors(corsOptions));
+// const corsOptions = {
+//   // origin: 'http://localhost:5173',
+//   origin: 'https://gigchain-frontend.vercel.app',
+//   optionsSuccessStatus: 200
+//   };
 
+// app.use(cors(corsOptions));
+
+const corsOptions = {
+  origin: "https://gigchain-frontend.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://gigchain-frontend.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {})
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+const userRoutes = require("./routes/users");
+app.use("/api/users", userRoutes);
 
-const userRoutes = require('./routes/users');
-app.use('/api/users', userRoutes);
-
-const gigRoutes = require('./routes/gig');
-app.use('/api/gig', gigRoutes);
-
+const gigRoutes = require("./routes/gig");
+app.use("/api/gig", gigRoutes);
