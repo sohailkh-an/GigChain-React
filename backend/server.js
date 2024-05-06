3626262e776954b8dedcffbc92d91b9308fbed1b
Sohail Khan <35258654+sohailkh-an@users.noreply.github.com>
2024-05-06 19:26:09 +0500

    Update server.js CORS URLs

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://gigchain-frontend.vercel.app"
+    req.headers.origin
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
