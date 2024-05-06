8c2dc29b25226782ab6b8fb5a67a555c9b87120d
Sohail Khan <Pindigamer@gmail.com>
2024-05-07 01:10:11 +0500

    Added gig details page and cleaned the navigation with improvements in css stylings

--- backend/server.js
+++ backend/server.js
@@ -7,20 +7,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-<<<<<<< HEAD
   origin:  'http://localhost:5173',
   methods: ["GET", "POST", "PUT", "DELETE"],
-=======
-  origin: "https://gigchain-frontend.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
->>>>>>> a6ab6f256e5637576b32ed800f6d8bcf6e691e8c
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-<<<<<<< HEAD
   res.setHeader(
     "Access-Control-Allow-Origin",
     req.headers.origin
@@ -42,9 +36,6 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", 'http://localhost:5173');
-=======
-  res.setHeader("Access-Control-Allow-Origin", "https://gigchain-frontend.vercel.app");
->>>>>>> a6ab6f256e5637576b32ed800f6d8bcf6e691e8c
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
