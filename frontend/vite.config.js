a9576a3674f88702ff41b33de91f167a1c0e9682
Sohail Khan <35258654+sohailkh-an@users.noreply.github.com>
2024-05-06 12:21:56 +0500

    Updated vite.config.js's local URL with the deployed URL from Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Replace with your backend server URL
+        target: 'https://gigchain-backend.vercel.app', // Replace with your backend server URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
