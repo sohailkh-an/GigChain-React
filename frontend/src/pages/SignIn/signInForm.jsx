67c0b1fc783aef53be861d6238362279ea8ecc61
Sohail Khan <Pindigamer@gmail.com>
2024-05-16 12:31:13 +0500

    chore: Update API URL in signInForm.jsx to use new backend server

--- frontend/src/pages/SignIn/signInForm.jsx
+++ frontend/src/pages/SignIn/signInForm.jsx
@@ -37,7 +37,7 @@ export default function SignInPage() {
   const handleSubmit = async (e) => {
   e.preventDefault();
   try {
-    const res = await axios.post('${import.meta.env.VITE_API_URL}/api/users/signin', userData);
+    const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/users/signin`, userData);
     const { token, user } = res.data;
     login({ token, user });
     console.log(res.data);
